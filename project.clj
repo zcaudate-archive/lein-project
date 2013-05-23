c0ef9798a47d3b0970aa368f4db0984157d9ef76
Chris Zheng <z@caudate.me>
2013-05-23 10:13:56 +1000

    change to lein project

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject lein-create "0.3.8-SNAPSHOT"
+(defproject lein-project "0.3.8-SNAPSHOT"
   :description "A Leiningen plugin for generating new projects based on templates."
   :dependencies [[stencil "0.3.0"]
                  [bultitude "0.1.7"]]
-  :url "https://github.com/Raynes/lein-newnew"
+  :url "https://github.com/zcaudate/lein-create"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in  :leiningen)
